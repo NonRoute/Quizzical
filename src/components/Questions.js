07c1fb22e3a1d4b16a8e257842b43c69791ec197
Chanon Rattanajaratkul <chanon45559@outlook.com>
2022-07-18 00:34:43 +0700

    Update style of hr

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -39,7 +39,7 @@ export default function Questions() {
     function shuffle(array) {
         let currentIndex = array.length, randomIndex;
         // While there remain elements to shuffle.
-        while (currentIndex != 0) {
+        while (currentIndex !== 0) {
             // Pick a remaining element.
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
